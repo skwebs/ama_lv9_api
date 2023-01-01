868575d5faeafb218f4d8761350a6efb4a98b52c
Satish Kumar Sharma <skwebserv@gmail.com>
2023-01-01 16:08:05 +0530

    Added lighthouse for access graphql feature in laravel.

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [StuParentController::class, 'index']);
+// Route::get('/', [StuParentController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
